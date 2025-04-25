4abbf95877b657952bd5cd55d75198146e81a766
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2025-04-25 09:53:33 -0300

    test: configure a in-memory sqlite database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,31 @@
 from typing import Generator
 
-from fastapi.testclient import TestClient
 import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import Engine, StaticPool, create_engine
 
+from vidaplus.models.config.base import Base
+from vidaplus.models.config.connection import DatabaseConnectionHandler
 from vidaplus.run import app
+from vidaplus.settings import Settings
 
 
 @pytest.fixture
-def client() -> Generator[TestClient, None, None]:
+def engine(monkeypatch: pytest.MonkeyPatch) -> Generator[Engine, None, None]:
+    monkeypatch.setenv('DATABASE_URL', 'sqlite:///:memory:')
+
+    engine = create_engine(Settings().DATABASE_URL, connect_args={'check_same_thread': False}, poolclass=StaticPool)
+
+    monkeypatch.setattr(DatabaseConnectionHandler, '_DatabaseConnectionHandler__create_engine', lambda x: engine)
+
+    Base.metadata.create_all(engine)
+
+    yield engine
+
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def client(engine: Engine) -> Generator[TestClient, None, None]:
     with TestClient(app) as client:
         yield client
