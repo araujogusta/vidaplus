e7222c34449e442ca1aebc267ec1fa3f4ef2e576
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2025-04-29 08:23:58 -0300

    feat(healthcare_professionals): add list appointments endpoint for healthcare professionals

--- vidaplus/controllers/healthcare_professionals.py
+++ vidaplus/controllers/healthcare_professionals.py
@@ -4,8 +4,11 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 
 from vidaplus.main.enums.roles import Roles
+from vidaplus.main.schemas.appointment import AppointmentSchema
 from vidaplus.main.schemas.user import PublicUserSchema, RequestCreateUserSchema
+from vidaplus.models.repositories.appointment_repository import AppointmentRepository
 from vidaplus.models.repositories.user_repository import UserRepository
+from vidaplus.services.appointment_service import AppointmentService
 from vidaplus.services.auth_service import AuthService
 from vidaplus.services.user_service import UserService
 
@@ -35,3 +38,13 @@ def get_healthcare_professional(
     repository = UserRepository()
     service = UserService(repository)
     return service.get_by_id(healthcare_professional_id)
+
+
+@router.get('/{healthcare_professional_id}/agendamentos', status_code=HTTPStatus.OK, response_model=list[AppointmentSchema])
+def get_healthcare_professional_appointments(
+    healthcare_professional_id: UUID, 
+    current_user: PublicUserSchema = Depends(AuthService.get_current_user),
+) -> list[AppointmentSchema]:
+    repository = AppointmentRepository()
+    service = AppointmentService(repository)
+    return service.get(professional_id=healthcare_professional_id)
