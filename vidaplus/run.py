0b71e60c0479014806520905a83c4a86bcd48f90
Gustavo Oliveira <gusta01.oliveira@gmail.com>
2025-05-21 12:03:16 -0300

    feat(beds): create bed endpoints and register on http server

--- vidaplus/run.py
+++ vidaplus/run.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, HTTPException, Request
 
-from vidaplus.controllers import appointments, auth, healthcare_professionals, patients, units
+from vidaplus.controllers import appointments, auth, beds, healthcare_professionals, patients, units
 from vidaplus.main.exceptions import ApplicationError
 
 app = FastAPI(title='SSGHSS VidaPlus')
 app.include_router(appointments.router)
 app.include_router(auth.router)
+app.include_router(beds.router)
 app.include_router(healthcare_professionals.router)
 app.include_router(patients.router)
 app.include_router(units.router)
